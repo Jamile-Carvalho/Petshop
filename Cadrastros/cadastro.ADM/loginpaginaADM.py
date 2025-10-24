19fc619dbbb74a01992faba1ec1d3ee319ced081
Maria Jamile de Carvalho Albuquerque <jamilicarvalho933@gmail.com>
2025-10-24 15:24:22 -0300

    Petshop

---
+++ Cadrastros/cadastro.ADM/loginpaginaADM.py
@@ -0,0 +1,119 @@
+usuario = []
+servicos = []
+perfil = []
+
+while perfil !='Administrador' and perfil !='Cliente':
+    print('Perfil Inválido,tente novamente')
+    perfil = input('Digite se você é Administrador ou Cliente: ')
+    if perfil == 'Administrador' and perfil =='Cliente':
+        print('Login concluido com sucesso!')
+
+
+while True:
+    print('Escolha uma das seguintes opções para prosseguir:')
+    print('1-Gerenciamente de Serviços')
+    print('2-Gerenciameto de Produtos')
+    print('3-Sair')
+    opcao = input('Digite uma das opções para continuar: ')
+    if (opcao == '3'):
+        break
+         
+    elif (opcao == '1'):
+        while True:
+            print('---------GERENCIADOR DE SERVIÇOS------------')
+            print('Escolha uma das seguintes opções para prosseguir:')
+            print('A-Cadrastrar Serviço')
+            print('B-Buscar/Listar Serviços')
+            print('C-Atualizar Serviço')
+            print('D-Remover Serviço')
+            print('E-Nenhuma das opções acima')
+            opcao_servicos = input('Escolha uma das seguintes opções para prosseguir:')
+            if (opcao_servicos == 'E'):
+                break
+
+            elif (opcao_servicos == 'A'):
+                print('---------NOVO SERVIÇO------------')
+                nomeServico =input('Digite o nome do novo serviço:  ')
+                preco = float(input('Digite o valor do novo serviço R$: '))
+                servicos.append([nomeServico, preco])
+                print(f'Produto {nomeServico} - R$:{preco} Cadastrado com sucesso.')
+
+            elif (opcao_servicos == 'B'):
+                print('---------LISTA DE SERVIÇOS------------')
+                for s in servicos:
+                    print(f'Serviço: {s[0]} | Preço: {s[1]}')
+
+            elif (opcao_servicos == 'C'):
+                print('---------ATUALIZAR LISTA DE SERVIÇOS------------')
+                for indice in range(len(servicos)):
+                    print(f'Serviço {indice} - Serviços {servicos[indice][0]}')
+
+                indice = int(input('Digite o indice do serviço que você deseja atualizar: '))
+                while indice < 0 or indice >= (len(servicos)):
+                     print('Indice Inválido.Tente novamente!')
+                     indice = int(input('Digite o indice do serviço que você deseja atualizar: '))
+
+                print(f'Servico Atual: {servicos[indice][0]}')
+                novo_nome = input('Digite o nome do novo serviço: ')
+                novo_preco = float(input('Digite o novo preço do serviço: '))
+
+                novaSublistaServicos = [novo_nome, novo_preco]
+                servicos[indice] = novaSublistaServicos
+                print('Serviço atualizado com sucesso!!')
+
+            elif (opcao_servicos == 'D'):
+                print('---------REMOVER SERVIÇOS------------')
+                for indice in range(len(servicos)):
+                    print(f'Serviço {indice} - Serviços {servicos[indice][0]}')
+
+                indice = int(input('Digite o indice do serviço que você deseja remover: '))
+                while indice < 0 or indice >= (len(servicos)):
+                    print('Indice Inválido.Tente novamente!')
+                    indice = int(input('Digite o indice do serviço que você deseja remover: '))
+
+                servicos.remove(servicos[indice])
+
+            else:
+                print('Erro.Escolha uma opção válida!')
+
+            print('---------------------------------')
+
+
+
+
+
+
+            
+
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+                
+
+
+        
+
