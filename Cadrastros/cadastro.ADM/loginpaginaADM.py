1fd428d400e59918010c5bbffcb53becf250a34f
Maria Jamile de Carvalho Albuquerque <jamilicarvalho933@gmail.com>
2025-10-26 15:01:58 -0300

    Atualização do Código

--- Cadrastros/cadastro.ADM/loginpaginaADM.py
+++ Cadrastros/cadastro.ADM/loginpaginaADM.py
@@ -1,5 +1,6 @@
 usuario = []
 servicos = []
+produtos = []
 perfil = []
 
 while perfil !='Administrador' and perfil !='Cliente':
@@ -34,14 +35,15 @@ while True:
             elif (opcao_servicos == 'A'):
                 print('---------NOVO SERVIÇO------------')
                 nomeServico =input('Digite o nome do novo serviço:  ')
+                descricao = input('Digite a descrição de seu serviço: ')
                 preco = float(input('Digite o valor do novo serviço R$: '))
-                servicos.append([nomeServico, preco])
-                print(f'Produto {nomeServico} - R$:{preco} Cadastrado com sucesso.')
+                servicos.append([nomeServico, descricao, preco])
+                print(f'Produto {nomeServico}\n Descrição: {descricao}\n e Preço - R$:{preco} \nCadastrado com sucesso.')
 
             elif (opcao_servicos == 'B'):
                 print('---------LISTA DE SERVIÇOS------------')
                 for s in servicos:
-                    print(f'Serviço: {s[0]} | Preço: {s[1]}')
+                    print(f'Serviço: {s[0]} | Descrição {s[1]} | Preço: {s[2]}')
 
             elif (opcao_servicos == 'C'):
                 print('---------ATUALIZAR LISTA DE SERVIÇOS------------')
@@ -55,9 +57,10 @@ while True:
 
                 print(f'Servico Atual: {servicos[indice][0]}')
                 novo_nome = input('Digite o nome do novo serviço: ')
+                nova_descricao = input('Digite a nova descrição de seu serviço: ')
                 novo_preco = float(input('Digite o novo preço do serviço: '))
 
-                novaSublistaServicos = [novo_nome, novo_preco]
+                novaSublistaServicos = [novo_nome, nova_descricao, novo_preco]
                 servicos[indice] = novaSublistaServicos
                 print('Serviço atualizado com sucesso!!')
 
@@ -77,7 +80,68 @@ while True:
                 print('Erro.Escolha uma opção válida!')
 
             print('---------------------------------')
+    
+    elif (opcao == '2'):
+        while True:
+            print('---------GERENCIADOR DE PRODUTOS------------')
+            print('Escolha uma das seguintes opções para prosseguir:')
+            print('A-Cadrastrar Produto')
+            print('B-Buscar/Listar Produtos')
+            print('C-Atualizar Produto')
+            print('D-Remover Produto')
+            print('E-Nenhuma das opções acima')
+            opcao_produtos = input('Escolha uma das seguintes opções para prosseguir:')
+            if (opcao_produtos == 'E'):
+                break
 
+            elif (opcao_produtos == 'A'):
+                print('---------NOVO PRODUTO------------')
+                nomeProduto =input('Digite o nome do novo produto:  ')
+                descricao = input('Digite a descrição de seu produto: ')
+                preco = float(input('Digite o valor do novo produto R$: '))
+                produtos.append([nomeProduto, descricao, preco])
+                print(f'Produto {nomeProduto}\n Descrição: {descricao}\n e Preço - R$:{preco} \nCadastrado com sucesso.')
+
+            elif (opcao_produtos == 'B'):
+                print('---------LISTA DE PRODUTOS------------')
+                for s in produtos:
+                    print(f'Produto: {s[0]} | Descrição {s[1]} | Preço: {s[2]}')
+
+            elif (opcao_produtos == 'C'):
+                print('---------ATUALIZAR LISTA DE PRODUTOS------------')
+                for indice in range(len(produtos)):
+                    print(f'Produto {indice} - Produtos {produtos[indice][0]}')
+
+                indice = int(input('Digite o indice do produto que você deseja atualizar: '))
+                while indice < 0 or indice >= (len(produtos)):
+                     print('Indice Inválido.Tente novamente!')
+                     indice = int(input('Digite o indice do produto que você deseja atualizar: '))
+
+                print(f'Produto Atual: {produtos[indice][0]}')
+                novo_nomeP = input('Digite o nome do novo produto: ')
+                novo_precoP = float(input('Digite o novo preço do produto: '))
+                nova_descricaoP = input('Digite a nova descrição de seu produto: ')
+
+                novaSublistaProdutos = [novo_nomeP, novo_precoP, nova_descricaoP]
+                produtos[indice] = novaSublistaProdutos
+                print('Produto atualizado com sucesso!!')
+
+            elif (opcao_produtos == 'D'):
+                print('---------REMOVER PRODUTOS------------')
+                for indice in range(len(produtos)):
+                    print(f'Produto {indice} - Produtos {produtos[indice][0]}')
+
+                indice = int(input('Digite o indice do produto que você deseja remover: '))
+                while indice < 0 or indice >= (len(produtos)):
+                    print('Indice Inválido.Tente novamente!')
+                    indice = int(input('Digite o indice do produto que você deseja remover: '))
+
+                produtos.remove(produtos[indice])
+            
+            else:
+                print('Erro.Escolha uma opção válida!')
+
+            print('---------------------------------')
 
 
 
--- Cadrastros/codigocompleto.py
+++ Cadrastros/codigocompleto.py
@@ -1,5 +1,6 @@
 usuario = []
 servicos = []
+produtos = []
 while True:
     print("Bem Vindo ao Pet & Cia")
     print("Escolha uma das seguintes opções para prosseguir:")
@@ -93,9 +94,10 @@ while True:
                     elif opcao_servicos == "A":
                         print("---------NOVO SERVIÇO------------")
                         nomeServico = input("Digite o nome do novo serviço:  ")
+                        descricao = input("Digite a descrição de seu serviço: ")
                         preco = float(input("Digite o valor do novo serviço R$: "))
-                        servicos.append([nomeServico, preco])
-                        print(f"Produto {nomeServico} - R$:{preco} Cadastrado com sucesso.")
+                        servicos.append([nomeServico, descricao, preco])
+                        print(f"Produto {nomeServico}, Descrição  {descricao} e Preço - R$:{preco} Cadastrado com sucesso.")
 
                     elif opcao_servicos == "B":
                         print("---------LISTA DE SERVIÇOS------------")
@@ -137,4 +139,64 @@ while True:
 
                     print("---------------------------------")
 
-                    
+            elif opcao == "2":
+                while True:
+                    print("---------GERENCIADOR DE PRODUTOS------------")
+                    print("Escolha uma das seguintes opções para prosseguir:")
+                    print("A-Cadrastrar Produto")
+                    print("B-Buscar/Listar Produtos")
+                    print("C-Atualizar Produto")
+                    print("D-Remover Produto")
+                    print("E-Nenhuma das opções acima")
+            opcao_produtos = input("Escolha uma das seguintes opções para prosseguir:")
+            if opcao_produtos == "E":
+                break
+
+            elif opcao_produtos == "A":
+                print("---------NOVO PRODUTO------------")
+                nomeProduto = input("Digite o nome do novo produto:  ")
+                descricao = input("Digite a descrição de seu produto: ")
+                preco = float(input("Digite o valor do novo produto R$: "))
+                produtos.append([nomeProduto, descricao, preco])
+                print(f"Produto {nomeProduto}\n Descrição: {descricao}\n e Preço - R$:{preco} \nCadastrado com sucesso.")
+
+            elif opcao_produtos == "B":
+                print("---------LISTA DE PRODUTOS------------")
+                for s in produtos:
+                    print(f"Produto: {s[0]} | Descrição {s[1]} | Preço: {s[2]}")
+
+            elif opcao_produtos == "C":
+                print("---------ATUALIZAR LISTA DE PRODUTOS------------")
+                for indice in range(len(produtos)):
+                    print(f"Produto {indice} - Produtos {produtos[indice][0]}")
+
+                indice = int(input("Digite o indice do produto que você deseja atualizar: "))
+                while indice < 0 or indice >= (len(produtos)):
+                    print("Indice Inválido.Tente novamente!")
+                    indice = int(input("Digite o indice do produto que você deseja atualizar: "))
+
+                print(f"Produto Atual: {produtos[indice][0]}")
+                novo_nomeP = input("Digite o nome do novo produto: ")
+                novo_precoP = float(input("Digite o novo preço do produto: "))
+                nova_descricaoP = input("Digite a nova descrição de seu produto: ")
+
+                novaSublistaProdutos = [novo_nomeP, novo_precoP, nova_descricaoP]
+                produtos[indice] = novaSublistaProdutos
+                print("Produto atualizado com sucesso!!")
+
+            elif opcao_produtos == "D":
+                print("---------REMOVER PRODUTOS------------")
+                for indice in range(len(produtos)):
+                    print(f"Produto {indice} - Produtos {produtos[indice][0]}")
+
+                indice = int(input("Digite o indice do produto que você deseja remover: "))
+                while indice < 0 or indice >= (len(produtos)):
+                    print("Indice Inválido.Tente novamente!")
+                    indice = int(input("Digite o indice do produto que você deseja remover: "))
+
+                produtos.remove(produtos[indice])
+
+            else:
+                print("Erro.Escolha uma opção válida!")
+
+            print("---------------------------------")
